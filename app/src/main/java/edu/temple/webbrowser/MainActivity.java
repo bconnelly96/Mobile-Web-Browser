03d05255e6cc9a9dc33a5620fb221eba53489cda
bconnelly <tug21498@temple.edu>
2018-10-31 12:43:54 -0400

    added buttons to actionbar

--- app/src/main/java/edu/temple/webbrowser/MainActivity.java
+++ app/src/main/java/edu/temple/webbrowser/MainActivity.java
@@ -2,6 +2,9 @@ package edu.temple.webbrowser;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.app_toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_items, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected (MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_add) {
+
+        } else if (id == R.id.action_forward) {
+
+        } else if (id == R.id.action_backward) {
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
